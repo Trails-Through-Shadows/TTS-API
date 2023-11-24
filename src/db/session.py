0ec4c100d26a86f060295b1c16e295e1465460ba
Kkobarii <barca.kovalska@gmail.com>
2023-11-24 21:57:08 +0100

    Added support for .env config

--- src/db/session.py
+++ src/db/session.py
@@ -1,3 +1,5 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -6,28 +8,36 @@ from .init_db import db_init
 # todo make config file or smthng
 #   plus session checking (any type of login)
 
-conn_params = {
-    'user': "communist",
-    'password': "yourPasswordIsMyPassword",
-    'host': "49.13.93.112",
-    'port': "3306",
-    "database": "communistBachelor"
-}
+# throw if .env file not exists
+if not os.path.exists("src/db/.env"):
+    raise Exception("No .env file in src/db/ directory")
+
+# read conn_params from .env file
+conn_params = {}
+with open("src/db/.env", "r") as f:
+    for line in f:
+        try:
+            key, value = line.split("=")
+            conn_params[key] = value.strip()
+        except ValueError:
+            continue
+
+# create db url
 db_url = ("mariadb+mariadbconnector://" +
           conn_params['user'] + ":" +
           conn_params['password'] + "@" +
           conn_params['host'] + ":" +
           conn_params["port"] + "/" +
           conn_params["database"])
-print("DB url:", db_url)
+print("DB url:  ", db_url)
 
-# db_url = "jdbc:mariadb://49.13.93.112:3306/communistBachelor" # this is from datagrip
-# db_url = "mariadb+mariadbconnector://49.13.93.112:3306/communistBachelor"
-
+# create engine and session
 engine = create_engine(db_url, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# init engine
 db_init(engine)
 
+# create session
 SessionLocal.configure(bind=engine)
 session = SessionLocal()
