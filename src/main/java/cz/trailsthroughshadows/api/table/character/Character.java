995fc198947c5fcb41033635849ab1c212b02d64
Kkobarii <barca.kovalska@gmail.com>
2023-11-28 23:16:53 +0100

    Added Character

---
+++ src/main/java/cz/trailsthroughshadows/api/table/character/Character.java
@@ -0,0 +1,51 @@
+package cz.trailsthroughshadows.api.table.character;
+
+import cz.trailsthroughshadows.api.table.campaign.Campaign;
+import cz.trailsthroughshadows.api.table.clazz.Clazz;
+import cz.trailsthroughshadows.api.table.hex.Hex;
+import cz.trailsthroughshadows.api.table.race.Race;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Random;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Character")
+public class Character {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "idCampaign")
+    private Campaign campaign;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "idClass")
+    private Clazz clazz;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "idRace")
+    private Race location;
+
+    @Column
+    private int level;
+
+    @Column
+    private String name;
+
+    @Column
+    private String playerName;
+
+    @Transient
+    private Hex hex;
+
+    public int roll() {
+        return new Random().nextInt(1, 21);
+    }
+}
