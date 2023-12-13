7bcfaebf74caab9f4e2a6ea1c0adb50cec4974cc
Kkobarii <barca.kovalska@gmail.com>
2023-12-13 12:08:30 +0100

    Added basic structure to methods (they dont do anything yet tho)

--- src/main/java/cz/trailsthroughshadows/algorithm/Dungeon.java
+++ src/main/java/cz/trailsthroughshadows/algorithm/Dungeon.java
@@ -2,6 +2,11 @@ package cz.trailsthroughshadows.algorithm;
 
 import cz.trailsthroughshadows.algorithm.entity.Entity;
 import cz.trailsthroughshadows.algorithm.location.LocationImpl;
+import cz.trailsthroughshadows.api.table.action.Action;
+import cz.trailsthroughshadows.api.table.action.attack.Attack;
+import cz.trailsthroughshadows.api.table.action.movement.Movement;
+import cz.trailsthroughshadows.api.table.action.restorecards.RestoreCards;
+import cz.trailsthroughshadows.api.table.action.skill.Skill;
 import cz.trailsthroughshadows.api.table.action.summon.Summon;
 import cz.trailsthroughshadows.api.table.character.Character;
 import cz.trailsthroughshadows.api.table.effect.Effect;
@@ -10,6 +15,7 @@ import cz.trailsthroughshadows.api.table.schematic.hex.Hex;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.util.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -74,5 +80,54 @@ public class Dungeon {
         }
 
         return targets;
+    }
+
+    public void evaluateMovement(Entity entity, Movement movement) {
+        log.info("Evaluating movement " + movement.toString() + " for " + entity.toString());
+
+    }
+
+    public void evaluateSummon(Entity entity, Summon summon, int range) {
+        log.info("Evaluating summon " + summon.toString() + " for " + entity.toString() + " with range " + range);
+
+    }
+
+    public void evaluateSummon(Entity entity, List<Pair<Summon, Integer>> summons) {
+        log.info("Evaluating summons " + summons.toString() + " for " + entity.toString());
+
+    }
+
+    public void evaluateSkill(Entity entity, Skill skill) {
+        log.info("Evaluating skill " + skill.toString() + " for " + entity.toString());
+
+    }
+
+    public void evaluateAttack(Entity entity, Attack attack) {
+        log.info("Evaluating attack " + attack.toString() + " for " + entity.toString());
+
+    }
+
+    public void evaluateRestoreCards(Entity entity, RestoreCards restoreCards) {
+        log.info("Evaluating restore cards " + restoreCards + " for " + entity.toString());
+
+    }
+
+    public void evaluateAction(Entity entity, Action action) {
+        log.info("Evaluating action " + action.toString() + " for " + entity.toString());
+        if (action.getMovement() != null) {
+            evaluateMovement(entity, action.getMovement());
+        }
+        if (action.getSummonActions() != null) {
+//            evaluateSummons(entity, action.getSummons());
+        }
+        if (action.getSkill() != null) {
+            evaluateSkill(entity, action.getSkill());
+        }
+        if (action.getAttack() != null) {
+            evaluateAttack(entity, action.getAttack());
+        }
+        if (action.getRestoreCards() != null) {
+            evaluateRestoreCards(entity, action.getRestoreCards());
+        }
     }
 }
