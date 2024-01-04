a02b52c009c02f1722195d050c6db6926229659e
Martin Korotwitschka <korotwitschkamartin@gmail.com>
2024-01-05 00:10:49 +0100

    parts now has Set of doors (doesnt mind bcs soon threre will be other table instead of HexDoor)

--- src/main/java/cz/trailsthroughshadows/api/table/schematic/part/Part.java
+++ src/main/java/cz/trailsthroughshadows/api/table/schematic/part/Part.java
@@ -3,15 +3,17 @@ package cz.trailsthroughshadows.api.table.schematic.part;
 import cz.trailsthroughshadows.api.table.schematic.hex.Hex;
 import cz.trailsthroughshadows.api.table.schematic.hex.HexDoor;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.JoinColumnsOrFormulas;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @Entity
@@ -20,6 +22,7 @@ import java.util.List;
 @Table(name = "Part")
 public class Part {
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -27,12 +30,11 @@ public class Part {
     @Column(name = "tag")
     private String tag;
 
-    @OneToMany(mappedBy = "key.idPart", fetch = FetchType.LAZY)
-    private Collection<Hex> hexes;
+    @OneToMany(mappedBy = "key.idPart")
+    private Set<Hex> hexes;
 
-//    @Query("SELECT h FROM HexDoor h WHERE :id = h.firstPart OR :id = h.secondPart")
-//    public List<HexDoor> hexDoors(@Param("id") int id);
-
+    @OneToMany(mappedBy = "firstPart")
+    private Set<HexDoor> firstPart;
 
     @Transient
     private int usages = 0;
--- src/main/java/cz/trailsthroughshadows/api/table/schematic/part/PartRepo.java
+++ src/main/java/cz/trailsthroughshadows/api/table/schematic/part/PartRepo.java
@@ -1,16 +1,21 @@
 package cz.trailsthroughshadows.api.table.schematic.part;
 
+import cz.trailsthroughshadows.api.table.schematic.hex.HexDoor;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PartRepo extends JpaRepository<Part, Integer> {
 
     @Override
-    @EntityGraph(attributePaths = {"hexes"})
+    @EntityGraph(attributePaths = {"hexes", "firstPart"})
     List<Part> findAll();
+
+    @Query("SELECT h FROM HexDoor h WHERE :id = h.firstPart OR :id = h.secondPart")
+    List<HexDoor> getHexDoors(@Param("id") int idPart);
 
     @Query("SELECT p FROM Part p")
     List<Part> findWithoutHexes();
