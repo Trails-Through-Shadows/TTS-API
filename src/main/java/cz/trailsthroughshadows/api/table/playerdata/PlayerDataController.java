f06bc07d518223e1e27d2cc3374ab4c09e778de6
rcMarty <korotwitschkamartin@gmail.com>
2024-01-19 18:42:04 +0100

    oh sh*t git

--- src/main/java/cz/trailsthroughshadows/api/table/playerdata/PlayerDataController.java
+++ src/main/java/cz/trailsthroughshadows/api/table/playerdata/PlayerDataController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/playerdatas/")
+@RequestMapping("/playerdata/")
 public class PlayerDataController {
     @Autowired
     private CharacterRepo repository;
