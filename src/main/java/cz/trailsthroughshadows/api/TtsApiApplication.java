6a051564ef56cff7f32a4025e8988a8c68a270c3
Firestone82 <mikpav@email.cz>
2024-01-26 22:52:53 +0100

    Implemented features requested by myself (:

--- src/main/java/cz/trailsthroughshadows/api/TtsApiApplication.java
+++ src/main/java/cz/trailsthroughshadows/api/TtsApiApplication.java
@@ -20,21 +20,22 @@ public class TtsApiApplication {
 //        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
 
         //.env file load
-        Dotenv dotenv = Dotenv.configure().directory(".config").load();
+//        Dotenv dotenv = Dotenv.configure().directory(".config").load();
+//
+//        Map<String, Object> env = new HashMap<>();
+//        env.put("spring.datasource.url", dotenv.get("DB_HOST") + ":" + dotenv.get("DB_PORT") + "/" + dotenv.get("DB"));
+//        env.put("spring.datasource.driver-class-name", dotenv.get("DB_DRIVER"));
+//        env.put("spring.datasource.username", dotenv.get("DB_USER"));
+//        env.put("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+//        //jdbc:mariadb://49.13.93.112:3306/tts_api
+//
+//        SpringApplication app = new SpringApplication(TtsApiApplication.class);
+//        app.setDefaultProperties(env);
+//        app.run(args);
+//        log.debug("Running with login: " + dotenv.get("DB_USER") + " -> " + dotenv.get("DB_PASSWORD"));
 
-        Map<String, Object> env = new HashMap<>();
-        env.put("spring.datasource.url", dotenv.get("DB_HOST") + ":" + dotenv.get("DB_PORT") + "/" + dotenv.get("DB"));
-        env.put("spring.datasource.driver-class-name", dotenv.get("DB_DRIVER"));
-        env.put("spring.datasource.username", dotenv.get("DB_USER"));
-        env.put("spring.datasource.password", dotenv.get("DB_PASSWORD"));
-        //jdbc:mariadb://49.13.93.112:3306/tts_api
 
-        SpringApplication app = new SpringApplication(TtsApiApplication.class);
-        app.setDefaultProperties(env);
-        app.run(args);
-        log.debug("Running with login: " + dotenv.get("DB_USER") + " -> " + dotenv.get("DB_PASSWORD"));
-
-
+        SpringApplication.run(TtsApiApplication.class, args);
         log.info("Docs running on http://localhost:8080/swagger-ui/index.html");
         log.info("API running on http://localhost:8080/");
     }
