e64e293f87dca428d7817bcccf0c5d82ff3d9572
rcMarty <korotwitschkamartin@gmail.com>
2024-02-27 23:20:37 +0100

    actions fixed and enemy not removing stuff fixed

--- src/main/java/cz/trailsthroughshadows/api/table/action/model/ActionDTO.java
+++ src/main/java/cz/trailsthroughshadows/api/table/action/model/ActionDTO.java
@@ -72,17 +72,10 @@ public class ActionDTO extends Validable {
     @OneToMany(fetch = FetchType.LAZY)
     @JoinColumn(name = "idAction")
     @JsonSerialize(using = LazyFieldsSerializer.class)
-    private List<SummonAction> summonActions = List.of();
+    private List<SummonAction> summonActions = new ArrayList<>();
 
     public void setSummonActions(List<SummonAction> actions) {
-        if (this.summonActions == null) {
-            this.summonActions = new ArrayList<>();
-        }
-        if (actions == null) {
-            this.summonActions = new ArrayList<>();
-        }
         this.summonActions.clear();
-
         if (actions != null) {
             this.summonActions.addAll(actions);
         }
